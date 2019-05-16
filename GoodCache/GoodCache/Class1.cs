55e19e8c3e39e26c09f8c8fa935620486dbbdb9c
Marius Žilėnas <m.zilenas@litrail.lt>
2019-05-16 16:32:03 +0300

    Su events.

--- GoodCache/GoodCache/Class1.cs
+++ GoodCache/GoodCache/Class1.cs
@@ -1,24 +1,26 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GoodCache
 {
-
-    public class CachedObject : AbstractCachedObject
+    public class CachedObject : ICacheable
     {
         private string Id { get; }
-        public override string GetId()
-        {
-            return Id;
-        }
+
         public CachedObject()
         {
             Id = Guid.NewGuid().ToString();
         }
+        public virtual string GetId()
+        {
+            return Id;
+        }
+        
         public override bool Equals(object obj)
         {            
             if (obj == null || GetType() != obj.GetType())
@@ -38,22 +40,14 @@ namespace GoodCache
         }
     }
 
-    public class AbstractCachedObject : ICacheable
-    {
-        public virtual string GetId()
-        {
-            throw new NotImplementedException();
-        }
-    }
-
     public interface ICacheable
     {
         string GetId();
     }
 
-    public class CacheEntry<ICacheable>
+    public class CacheEntry<T> where T : ICacheable
     {
-        public ICacheable Value { get; private set; }
+        public T Value { get; private set; }
         public DateTime CachedOn { get; private set; }
         
         public void Reset()
@@ -61,73 +55,201 @@ namespace GoodCache
             CachedOn = DateTime.Now;
         }
 
-        public void ResetTo(ICacheable cacheable)
+        public void ResetTo(T cacheable)
         {
             Value = cacheable;
             Reset();
         }
 
-        public CacheEntry(ICacheable o, DateTime when)
+        public CacheEntry(T o, DateTime when)
         {
             Value = o;
             CachedOn = when;
         }
+
+        public string GetId() => Value.GetId();
     }
-    public class Cache : ICollection
+    public class Cache<T> : ICollection<T> where T : ICacheable
     {
-        Dictionary<string, CacheEntry<ICacheable>> Entries { get; }
+        private Dictionary<string, CacheEntry<T>> Entries { get; }
+        public ICacheSweeper CacheSweeper { get; private set; }
 
-        public int Count => ((ICollection)Entries).Count;
+        public int Count => Entries.Count();
 
-        public object SyncRoot => ((ICollection)Entries).SyncRoot;
+        public bool IsReadOnly => ((ICollection<T>)Entries).IsReadOnly;        
 
-        public bool IsSynchronized => ((ICollection)Entries).IsSynchronized;
-
-        public CacheEntry<ICacheable> Get(string key)
+        public T Get(string key)
         {
-            CacheEntry<ICacheable> cacheEntry = null;
-            if (Entries.TryGetValue(key, out cacheEntry))
+            if (Entries.TryGetValue(key, out CacheEntry<T> cacheEntry))
+            {
+                return cacheEntry.Value;
+            }
+            else
+            {
+                return default;
+            }
+        }
+        private CacheEntry<T> Entry(T o)
+        {
+            if (Entries.TryGetValue(o.GetId(), out CacheEntry<T> cacheEntry))
             {
                 return cacheEntry;
             }
             else
             {
-                return null;
+                return default;
             }
         }
-        public void AddOrUpdate(ICacheable o)
+
+        public void AddOrUpdate(T o)
         {
-            CacheEntry<ICacheable> ce = null;
-            if (Entries.TryGetValue(o.GetId(), out ce))
+            CacheEntry<T> entry = Entry(o);            
+            if (entry != null)
             {
-                if (ce.Equals(o))
+                if (entry.Value.Equals(o))
                 {
-                    ce.Reset();
+                    entry.Reset();
                 }
                 else
                 {
-                    ce.ResetTo(o);
+                    entry.ResetTo(o);
                 }
             }
             else
             {
-                Entries.Add(o.GetId(), new CacheEntry<ICacheable>(o,DateTime.Now));
+                Entries.Add(o.GetId(), new CacheEntry<T>(o,DateTime.Now));
+            }
+        } 
+
+        public void AddOrUpdate(ICollection<T> cacheables)
+        {
+            foreach (var c in cacheables)
+            {
+                AddOrUpdate(c);
+            }
+        }    
+
+        bool ICollection<T>.Remove(T item) => Entries.Remove(item.GetId());
+
+        public void Add(T item)
+        {
+            if (Contains(item))
+            {
+                throw new InvalidOperationException("Already exists");
+            }
+            else
+            {
+                AddOrUpdate(item);
             }
         }
 
-        public void CopyTo(Array array, int index)
-        {
-            ((ICollection)Entries).CopyTo(array, index);
-        }
+        public void Clear() => Entries.Clear();        
 
-        public IEnumerator GetEnumerator()
-        {
-            return ((ICollection)Entries).GetEnumerator();
-        }
+        public bool Contains(T item) => Get(item.GetId()) != null;
+
+        IEnumerator IEnumerable.GetEnumerator() => ((IEnumerable)Entries).GetEnumerator();
+        public void CopyTo(T[] array, int arrayIndex) => ((ICollection)Entries).CopyTo(array, arrayIndex);
+
+        public IEnumerator<T> GetEnumerator() => ((IEnumerable<T>)Entries).GetEnumerator();
 
         public Cache()
         {
-            Entries = new Dictionary<string, CacheEntry<ICacheable>>();
+            Entries = new Dictionary<string, CacheEntry<T>>();
         }
+    }
+
+    public interface ICacheSweeper
+    {
+        void Run();
+    }
+
+    public class CacheSweeper<T> : ICacheSweeper
+    {
+        public ISweepingStrategy<T> SweepingStrategy { get; private set; }
+        public ICollection<T> SweepOver { get; private set; }
+        public CacheSweeper(ICollection<T> sweepOver)
+        {
+            SweepOver = sweepOver;
+        }
+        public void Run()
+        {
+            foreach (T item in SweepOver)
+            {
+                if (SweepingStrategy.ShouldRemove(item))
+                {
+                    SweepOver.Remove(item);
+                }
+            }            
+        }
+    }
+
+    public interface ISweepingStrategy<T>
+    {
+        bool ShouldRemove(T cacheable);
+    }
+
+    public class StartSweepingEventArgs : EventArgs { }
+    public class CacheKeeper
+    {     
+        
+        private IKeepingStrategy KeepingStrategy { get; set; }
+
+        public CacheKeeper(Cache<ICacheable> cache) : this(cache, new KeepingStrategyTimed(TimeSpan.FromSeconds(3)))
+        {
+        }
+
+        public CacheKeeper(Cache<ICacheable> cache, IKeepingStrategy keepingStrategy)
+        {
+            Cache = cache;
+            KeepingStrategy = keepingStrategy;            
+        }
+
+        Cache<ICacheable> Cache { get; set; }
+        public void Run()
+        {
+            KeepingStrategy.Run(Cache);
+        }
+    }
+
+    internal class KeepingStrategyTimed : KeepingStrategy
+    {
+        private TimeSpan timeSpan;
+
+        public KeepingStrategyTimed(TimeSpan timeSpan)
+        {
+            this.timeSpan = timeSpan;
+        }
+
+        public void Run(Cache<ICacheable> cache)
+        {
+            var stopwatch = new Stopwatch();
+            var ends = false;
+            while (!ends)
+            {
+                while (stopwatch.ElapsedMilliseconds < timeSpan.TotalMilliseconds) { }
+                cache.
+            }
+        }
+        
+    }
+
+    public abstract class KeepingStrategy : IKeepingStrategy
+    {        
+        public virtual void OnTimerElapsed(StartSweepingEventArgs args)
+        {
+            TimerElapsed?.Invoke(this, args);
+        }
+
+        public EventHandler<StartSweepingEventArgs> TimerElapsed;
+
+        public void Run(Cache<ICacheable> cache)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IKeepingStrategy
+    {        
+        void Run(Cache<ICacheable> cache);
     }
 }
--- GoodCache/GoodCacheTests/UnitTest1.cs
+++ GoodCache/GoodCacheTests/UnitTest1.cs
@@ -1,14 +1,48 @@
 using NUnit.Framework;
 using GoodCache;
+using System.Collections.Generic;
 
 namespace Tests
 {
     public class Tests
     {
+
+        private IList<ICacheable> CachedObjects(int count)
+        {
+            var list = new List<ICacheable>();
+            for (int i = 0; i < 10000; i++)
+            {
+                var co = new CachedObject();
+                list.Add(co);
+            }
+            return list;
+        }
+
         [SetUp]
         public void Setup()
         {
 
+        }
+
+        [Test]
+        public void TestGet()
+        {
+            var gc = new Cache<CachedObject>();
+            
+            var ck = new CacheKeeper(gc,);
+            var list = CachedObjects(10000);
+
+            foreach(var item in list)
+            {
+                gc.AddOrUpdate(item);
+            }
+
+            for (int i = 0; i< 10;i++)
+            {
+                var item = list[i];
+                var co =  (CachedObject)gc.Get(item.GetId()).Value;
+                Assert.That(item, Is.EqualTo(co));
+            }
         }
 
         [Test]
@@ -22,11 +56,22 @@ namespace Tests
         }
 
         [Test]
-        public void CreateCache()
+        public void TestCreateCache()
         {
             var gc = new Cache();
             Assert.That(gc, Is.Empty);
         }
 
+        [Test]
+        public void TestRemove()
+        {
+            var gc = new Cache();
+            var list = CachedObjects(10000);
+            gc.AddOrUpdate(list);
+            for (int i = 0; i < list.Count; i++)
+            {
+                gc.Remove(list[i]);
+            }
+        }
     }
 }
